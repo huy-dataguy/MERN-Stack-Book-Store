7943eba433d114660427fe6e9736b3db60fa8dfc
huy-dataguy <quochuy.working@gmail.com>
2025-03-10 16:50:36 +0700

    #1 add heart, cart, and avatar, searchbar icons to taskbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import { HiMiniBars3CenterLeft } from "react-icons/hi2";
+import { TiShoppingCart } from "react-icons/ti";
+import { HiMiniBars3CenterLeft, HiSparkles } from "react-icons/hi2";
+import { HiUserCircle } from "react-icons/hi2";
+import { HiOutlineHeart } from "react-icons/hi2";
 import { IoIosSearch } from "react-icons/io";
 import { Link } from 'react-router-dom';
+import avatarImage from "../assets/avatar.png"
 function Navbar() {
+  const currentUser = true;
   return (
     <header className='max-w-screen-2xl mx-auto px-4 py-6'>
       <nav className='flex justify-between items-center'>
@@ -11,18 +16,42 @@ function Navbar() {
             <HiMiniBars3CenterLeft className="size-6" />
 
           </Link>
-          <div className="relative ">
+          {/* search input */}
+          <div className="relative sm:w-72 w-40 space-x-2">
             <Link >
-              <IoIosSearch className="size-6" />
-              <input type="text" placeholder="Search here" />
+              <IoIosSearch className="absolute inline-block left-2 inset-y-1" />
+              <input type="text" placeholder="Search here"
+                className="bg-gray-200 w-full md:px-8 rounded-md focus:outline-none" />
             </Link>
           </div>
 
         </div>
 
         {/* right side */}
-        <div>
-          <div>Nav items</div>
+        <div className=" relative flex items-center md:space-x-3 space-x-2">
+
+          <div>
+            {
+              currentUser ? <>
+              <button>
+                <img src={avatarImage} alt="" className="size-7 rounded-full "></img>
+              </button>
+              </> : <Link to="/login">
+                <HiUserCircle className="size-6" />
+              </Link>
+            }
+          </div>
+
+          <button className="hidden sm:block md:border-hidden bg-white">
+            <HiOutlineHeart className="size-6" />
+          </button>
+
+          <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-md">
+            <TiShoppingCart className="size-6" />
+            <span>0</span>
+          </Link>
+
+
 
         </div>
       </nav>
