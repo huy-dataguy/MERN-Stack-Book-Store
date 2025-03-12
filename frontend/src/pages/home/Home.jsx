155d2aa97c0040fe5375fd44ac2cfe6f512328c4
huy-dataguy <quochuy.working@gmail.com>
2025-03-12 12:06:05 +0700

    #3 finish create top sellers section in home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Banner from './Banner'
+import TopSellers from './TopSellers'
 
 const Home = () => {
   return (
-    <Banner/>
+    <>
+      <Banner />
+      <TopSellers />
+    </>
+
   )
 }
+
+
 
 export default Home
