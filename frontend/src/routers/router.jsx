dd0df08205f67272a2a3a36f75f62768e80f7867
Quoc Huy <quochuy.working@gmail.com>
2025-06-10 02:34:41 +0700

    #26 finish admin dashboard with add delete edit book

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -9,6 +9,14 @@ import SingleBook from "../pages/books/SingleBook";
 import PrivateRoute from "./PrivateRoute";
 import OrderPage from "../pages/books/OrderPage";
 import AdminLogin from "../components/AdminLogin";
+import AdminRoute from "./AdminRoute";
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
+import Dashboard from "../pages/dashboard/Dashboard";
+import AddBook from "../pages/dashboard/addBook/AddBook";
+import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
+import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
+import UserDashboard from "../pages/dashboard/users/UserDashboard";
+
 const router = createBrowserRouter([
     {
       path: "/",
@@ -50,11 +58,43 @@ const router = createBrowserRouter([
           element: <SingleBook/> 
         },
         {
-          path: "/admin",
-          element: <AdminLogin/>
-        },
+          path: "/user-dashboard",
+          element: <PrivateRoute><UserDashboard/></PrivateRoute>
+        }
       ]
     },
+      {
+        path: "/admin",
+        element: <AdminLogin/>
+      },
+      {
+        path: "/dashboard",
+        element: <AdminRoute><DashboardLayout/></AdminRoute>,
+        children:[
+          {
+            path: "",
+            element: <AdminRoute><Dashboard/></AdminRoute>
+          },
+          {
+            path: "add-new-book",
+            element: <AdminRoute>
+              <AddBook/>
+            </AdminRoute>
+          },
+          {
+            path: "edit-book/:id",
+            element: <AdminRoute>
+              <UpdateBook/>
+            </AdminRoute>
+          },
+          {
+            path: "manage-books",
+            element: <AdminRoute>
+              <ManageBooks/>
+            </AdminRoute>
+          }
+        ]
+      }
   ]);
   
 export default router;
