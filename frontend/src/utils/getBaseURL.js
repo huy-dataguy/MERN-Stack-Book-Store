763ad04a02708a4672413f26ca880baa3c69dc2a
Nguyen Quoc Huy <150227535+huy-dataguy@users.noreply.github.com>
2025-06-10 19:59:07 +0700

    Update getBaseURL.js

--- frontend/src/utils/getBaseURL.js
+++ frontend/src/utils/getBaseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
     // return 'http://localhost:3000';
-    return 'https://book-store-eight-black.vercel.app/';
+    return 'https://book-store-eight-black.vercel.app';
 }
 export default getBaseUrl;
